0159ede844b1f7f7b292da0027d2be73afb004eb
ali-alhamdou <91735598+ali-alhamdou@users.noreply.github.com>
2023-11-13 11:42:45 +0300

    Admin panel design

---
+++ HospitalAutomation/Controllers/AdminPanelController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalAutomation.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Departments()
+        {
+            return View();
+        }
+        public IActionResult Doctors()
+        {
+            return View();
+        }
+        public IActionResult Ships()
+        {
+            return View();
+        }
+        public IActionResult Patients()
+        {
+            return View();
+        }
+        public IActionResult Reservations()
+        {
+            return View();
+        }
+    }
+}
