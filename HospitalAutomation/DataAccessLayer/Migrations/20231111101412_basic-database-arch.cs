e4efe09b409e6faa730548dea0eb85dd3d411d0e
ali-alhamdou <91735598+ali-alhamdou@users.noreply.github.com>
2023-11-28 13:52:07 +0300

    Completed Department and Doctor Page For AdminPanel

---
+++ HospitalAutomation/DataAccessLayer/Migrations/20231111101412_basic-database-arch.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class basicdatabasearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepartmentName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DepartmentAbout = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.DepartmentID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patients",
+                columns: table => new
+                {
+                    PatientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PatientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PatientBirthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PatientAbout = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patients", x => x.PatientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ships",
+                columns: table => new
+                {
+                    ShipID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ShipStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ShipEnd = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BreakStart = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BreakEnd = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ships", x => x.ShipID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    DoctorID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DoctorName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorAbout = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShipID = table.Column<int>(type: "int", nullable: false),
+                    DepartmentID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.DoctorID);
+                    table.ForeignKey(
+                        name: "FK_Doctors_Departments_DepartmentID",
+                        column: x => x.DepartmentID,
+                        principalTable: "Departments",
+                        principalColumn: "DepartmentID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Doctors_Ships_ShipID",
+                        column: x => x.ShipID,
+                        principalTable: "Ships",
+                        principalColumn: "ShipID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservations",
+                columns: table => new
+                {
+                    ReservationID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ReservationDay = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReservationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DoctorID = table.Column<int>(type: "int", nullable: false),
+                    PatientID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservations", x => x.ReservationID);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Doctors_DoctorID",
+                        column: x => x.DoctorID,
+                        principalTable: "Doctors",
+                        principalColumn: "DoctorID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservations_Patients_PatientID",
+                        column: x => x.PatientID,
+                        principalTable: "Patients",
+                        principalColumn: "PatientId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_DepartmentID",
+                table: "Doctors",
+                column: "DepartmentID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_ShipID",
+                table: "Doctors",
+                column: "ShipID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_DoctorID",
+                table: "Reservations",
+                column: "DoctorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservations_PatientID",
+                table: "Reservations",
+                column: "PatientID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reservations");
+
+            migrationBuilder.DropTable(
+                name: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Patients");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Ships");
+        }
+    }
+}
---
+++ HospitalAutomation/EntityLayer/Concrete/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Doctor
+    {
+        [Key]
+        public int DoctorID { get; set; }
+        public string DoctorName { get; set; }
+        public string DoctorAbout { get; set; }
+        public int ShipID { get; set; }
+        public Ship Ship { get; set; }
+        public int DepartmentID { get; set; }
+        public Department Department { get; set; }
+        public List<Reservation> Reservations { get; set; }
+    }
+}
